fe308fd8bdfbab4c5bbb60abb14be3e91386b3d6
Mariano Zanetto <80788269+MarZanett@users.noreply.github.com>
2022-03-09 11:38:13 -0300

    key checkout update

--- src/routes/checkOut.routes.js
+++ src/routes/checkOut.routes.js
@@ -7,7 +7,7 @@ const { checkOut } = require("../controllers/checkOut.controller.js");
 const router = express();
 
 const stripe = new Stripe(
-  "sk_test_51KZSeoFbNejxpP0on5jicw5JshREByqXE33i6mzAUMQq2VaLvaMk6OdAWqgEnygbtN71JZwMpdhuZLsUPamCQXPw009celed5X"
+  "sk_test_51KbQVvKlOgZYCviaM26mtACmLWGQDttlg2ZxMGKJonC3OfVNKYzxWXbeMilMKjCsk48S7yLrQayzk7DG5ZORFWBS00UQUWQ6TU"
 );
 
 router.use(cors({ origin: "http://localhost:3000" }));
