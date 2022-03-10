bd1c2a07134603f8b88c2c7deb94fef2cf3c3e04
Mariano Zanetto <80788269+MarZanett@users.noreply.github.com>
2022-03-09 21:30:37 -0300

    Filters updated

--- src/controllers/filterByGenre.controller.js
+++ src/controllers/filterByGenre.controller.js
@@ -1,59 +1,42 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-let genrArray = [];
 
 const filterByGenre = async (req, res) => {
   const { genr } = req.query;
-  if (!genrArray.includes(genr)) {
-    genrArray.push(genr);
-  }
-  //  console.log(genrArray);
+
   try {
-    if (genrArray.length >= 1) {
-      const genreFilter = await prisma.game.findMany({
+    if (genr) {
+      const products = await prisma.productsKey.findMany({
         include: {
-          genres: true,
-          platforms: true,
-          productKey: true,
-          region: true,
+          game: {
+            include: {
+              genres: true,
+            },
+          },
         },
       });
-      const formatDBGames = genreFilter.map((data) => {
-        return {
-          id: data.id,
-          name: data.name,
-          released: data.released_at?.toISOString().split("T")[0],
-          img: data.image,
-          trailer: data.trailer,
-          description: data.description,
-          genres: data.genres.map((genre) => genre.name),
-          platform: data.platforms?.map((plat) => plat.name),
-          productKey: data.productKey?.map((pkey) => pkey.key),
-        };
-      });
-      //   console.log(formatDBGames);
-let filteredGames = []
+// console.log(products)
 
-      for (let i = 0; i < genrArray.length; i++) {
-        let ele = genrArray[i];
-        // console.log(ele)
-        for (let j = 0; j < formatDBGames.length; j++) {
-          if (formatDBGames[j].genres.includes(ele)) {
-            filteredGames.push(formatDBGames[j]);
-          }
+      let filteredProducts = []
+
+        for (let i = 0; i < products.length; i++) {
+            let arrayGenre = products[i].game.genres
+            for (let j = 0; j < arrayGenre.length; j++) {
+                
+                if (arrayGenre[j].name === genr) {
+                  filteredProducts.push(products[i].game);
+                }
+    
+            }
         }
-      }
-      res.json(filteredGames);
-    } else {
-      res.json("Games not found");
-    }
+      
 
+      res.status(200).json(filteredProducts);
+    }
   } catch (error) {
     console.log(error);
-    res.status(500).json(error.message);
   }
 };
-
 
 module.exports = {
   filterByGenre,
--- src/controllers/filtersAcum.controller.js
+++ src/controllers/filtersAcum.controller.js
@@ -3,7 +3,7 @@ const prisma = new PrismaClient();
 
 const filterAcum = async (req, res) => {
   const { genr, plat, regi } = req.query;
-  //console.log(plat);
+  // console.log(plat);
   try {
     const allDBProducts = await prisma.productsKey.findMany({
       include: {
@@ -13,25 +13,53 @@ const filterAcum = async (req, res) => {
     });
 
     let filtered = allDBProducts;
+
+    const filterPlat = (array, plat) => {
+      if (typeof plat === "string") {
+        array = array.filter((g) => 
+          g.platform.name.includes(plat)
+        );
+        return array;
+      }
+    };
+
+    if (plat) {
+      if (typeof plat === "string") {
+        filtered = filterPlat(filtered, plat);
+      }
+      if(Array.isArray(plat)) {
+        let arreglo = []
+        for (let i = 0; i < plat.length; i++) {
+          arreglo.push(...filterPlat(filtered,plat[i]))
+          
+        }
+        filtered = arreglo
+      }
+    }
+
+    res.json(filtered);
+
+
+
+
     //console.log(filtered)
 
-    if (plat && !Array.isArray(plat)) {
-      filtered = filtered.filter((p) =>
-        p.platform.name.toLowerCase().includes(plat.toLowerCase())
-      );
-    } else if (Array.isArray(plat)) {
-      filtered = filtered.filter((g) => {
-        plat.map((p) => {
-          g.platform.name.toLowerCase().includes(p.toLowerCase());
-        });
-      });
-    }
+    // if (plat && !Array.isArray(plat)) {
+    //   filtered = filtered.filter((p) =>
+    //     p.platform.name.toLowerCase().includes(plat.toLowerCase())
+    //   );
+    // } else if (Array.isArray(plat)) {
+    //   filtered = filtered.filter((g) => {
+    //     // plat.map((p) => {});
+    //     g.platform.name.toLowerCase().includes(plat.pop().toLowerCase());
+    //   });
+    // }
 
-    if (genr) {
-      filtered = filtered.filter((g) =>
-        g.genres.map((g) => g.name.toLowerCase()).includes(genr.toLowerCase())
-      );
-    }
+    // if (genr) {
+    //   filtered = filtered.filter((g) =>
+    //     g.genres.map((g) => g.name.toLowerCase()).includes(genr.toLowerCase())
+    //   );
+    // }
 
     //   if (store) {
     //     filtered = filtered.filter((g) =>
@@ -41,7 +69,7 @@ const filterAcum = async (req, res) => {
     //     );
     //   }
 
-    res.json(filtered);
+    // res.json(filtered);
   } catch (error) {
     console.log(error);
   }
@@ -50,3 +78,36 @@ const filterAcum = async (req, res) => {
 module.exports = {
   filterAcum,
 };
+
+// const filter = (array, platform) => {
+//   if (typeof platform === "string") {
+//     array = array.filter((g) =>
+//       g.platforms
+//         .map((p) => p.platform.name.toLowerCase())
+//         .includes(platform.toLowerCase())
+//     );
+//     return array;
+//   }
+// };
+
+// if (platform) {
+//   if (typeof platform === "string") {
+//     filtered = filtered.filter((g) =>
+//       g.platforms
+//         .map((p) => p.platform.name.toLowerCase())
+//         .includes(platform.toLowerCase())
+//     );
+//   }
+//   // if (Array.isArray(platform)) {
+//   //   while(platform.length) {
+//   //     filtered = filtered.filter(g => g.platforms.map(p => p.platform.name.toLowerCase()).includes(platform.pop()))
+//   //   }
+//   // }
+//   if (Array.isArray(platform)) {
+//     let array = [];
+//     for (let i = 0; i < platform.length; i++) {
+//       array.push(...filter(filtered, platform[i]));
+//     }
+//     filtered = array;
+//   }
+// }
