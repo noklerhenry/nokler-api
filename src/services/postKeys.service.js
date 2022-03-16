bf3ce44b3bf7d16568804b13ccfbbf16c3be7d55
Gonzalo Soria <gonzalosoria.sg@gmail.com>
2022-03-16 09:24:06 -0300

    multiple keys to one product update

--- src/services/postKeys.service.js
+++ src/services/postKeys.service.js
@@ -2,20 +2,25 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 const newKeys = async (product, key) => {
-    
-    const createKeys = key.map(k => {
-        return {
-            value:k,
-            productId: Number(product.id)  
+    try {
+        if(product && key) {
+            const keys = key.map(k => {
+                return {
+                    value:k,
+                    productId: Number(product.id)  
+                }
+            })    
+        
+            const createKeys = await prisma.key.createMany({
+                data:keys,        
+            }) 
+        
+            return createKeys
         }
-    })    
-
-    const newKeys = await prisma.key.createMany({
-        data:createKeys,        
-    }) 
-
-    console.log(newKeys)
-    return newKeys
+    } catch (error) {
+        console.log(error)
+    }
+    
 }
 
 module.exports = {
