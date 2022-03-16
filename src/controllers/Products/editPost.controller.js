3c021ec489f0a12822604f17aa6605650b55d357
Mariano Zanetto <80788269+MarZanett@users.noreply.github.com>
2022-03-16 15:38:23 -0300

    me pide comitear para hacer merge con dev

--- src/controllers/Products/editPost.controller.js
+++ src/controllers/Products/editPost.controller.js
@@ -1,41 +1,51 @@
 const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient()
-
+const prisma = new PrismaClient();
 
 const editPost = async (req, res) => {
-    const { id } = req.params
-    const { updateValues } = req.body
+  const { id } = req.params;
+  const { updateValues } = req.body;
 
-    try {
-        const productToEdit = await prisma.productsKey.findUnique({
-            where: { 
-                id: Number(id) 
-            }
-        })
-        if(productToEdit) {
-            const productEdited = await prisma.productsKey.update({
-                where: { id: Number(id) },
-                data: {
-                    price: updateValues.price === '' ?  productToEdit.price : Number(updateValues.price) ,
-                    key: updateValues.key === '' ? productToEdit.key :  updateValues.key  ,
-                    region: updateValues.region === '' ?  productToEdit.region : updateValues.region ,
-                    storeId: updateValues.storeId === '' ? productToEdit.storeId : Number(updateValues.storeId) ,
-                    platformId: updateValues.platformId === '' ? productToEdit.platformId : Number(updateValues.platformId)
-                }
-            })             
-            console.log('Update succes!')
-            res.status(200).json(productEdited)        
-        } else {
-            console.log("Invalid ID product")
-            res.status(403).send("The product ID is incorrect, enter a valid ID.")
-        }
-    } catch (error) {
-        console.log(error)
-        res.status(500).json(error.message)        
+  try {
+    const productToEdit = await prisma.productsKey.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+    if (productToEdit) {
+      const productEdited = await prisma.productsKey.update({
+        where: { id: Number(id) },
+        data: {
+          price:
+            updateValues.price === ""
+              ? productToEdit.price
+              : Number(updateValues.price),
+          key: updateValues.key === "" ? productToEdit.key : updateValues.key,
+          region:
+            updateValues.region === ""
+              ? productToEdit.region
+              : updateValues.region,
+          storeId:
+            updateValues.storeId === ""
+              ? productToEdit.storeId
+              : Number(updateValues.storeId),
+          platformId:
+            updateValues.platformId === ""
+              ? productToEdit.platformId
+              : Number(updateValues.platformId),
+        },
+      });
+      console.log("Update succes!");
+      res.status(200).json(productEdited);
+    } else {
+      console.log("Invalid ID product");
+      res.status(403).send("The product ID is incorrect, enter a valid ID.");
     }
-}
-
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error.message);
+  }
+};
 
 module.exports = {
-    editPost
+  editPost,
 };
