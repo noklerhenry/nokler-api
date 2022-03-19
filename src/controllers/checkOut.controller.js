90580e31ecfc4e92071fcab5466971c94a09a55d
Gonzalo Soria <gonzalosoria.sg@gmail.com>
2022-03-19 18:31:34 -0300

    fix controller

--- src/controllers/checkOut.controller.js
+++ src/controllers/checkOut.controller.js
@@ -9,20 +9,20 @@ const { sendMail } = require('../services/sendEmail.service');
 const checkOut = async (req, res) => {
     const { amount, id, cart } = req.body;  
         
-    const gamesPurchased = cart.map(games => {
-      return {
-        name: games.game.name,
-        gameId: games.gameId,
-        platform: games.platform.name,
-        store: games.store.name,
-        region: games.region,
-        price: games.price,
-        quantity: games.quantity,
-        userId: games.userId,
-        key: games.key.slice(0,games.quantity)
+    // const gamesPurchased = cart.map(games => {
+    //   return {
+    //     name: games.game.name,
+    //     gameId: games.gameId,
+    //     platform: games.platform.name,
+    //     store: games.store.name,
+    //     region: games.region,
+    //     price: games.price,
+    //     quantity: games.quantity,
+    //     userId: games.userId,
+    //     key: games.key.slice(0,games.quantity)
         
-      }
-    })
+    //   }
+    // })
 
     const userId = cart[0].userId            
     //  console.log(gamesPurchased)
