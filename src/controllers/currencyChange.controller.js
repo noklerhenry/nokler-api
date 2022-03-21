4dd9bcdc7df8e7c78dd7de19ee466d5430cc5151
Gonzalo Soria <gonzalosoria.sg@gmail.com>
2022-03-21 12:11:21 -0300

    currency rates service

--- src/controllers/currencyChange.controller.js
+++ src/controllers/currencyChange.controller.js
@@ -1,18 +1,24 @@
 const { currencyRates } = require('../services/currencyApi.service')
 
 const changeCurrency = async (req, res) => {
-    const ratesApi = await currencyRates()
-
-    const conversionRates = {
-        USD: ratesApi.USD,
-        ARS: ratesApi.ARS,
-        EUR: ratesApi.EUR,
-        JPY: ratesApi.JPY,
-        GBP: ratesApi.GBP,
-        AUD: ratesApi.AUD
+    try {
+        const ratesApi = await currencyRates()
+    
+        const conversionRates = {
+            USD: ratesApi.USD,
+            ARS: ratesApi.ARS,
+            EUR: ratesApi.EUR,
+            JPY: ratesApi.JPY,
+            GBP: ratesApi.GBP,
+            AUD: ratesApi.AUD
+        }
+    
+        res.status(200).send(conversionRates)
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(error)       
     }
-
-    res.status(200).send(conversionRates)
 
 };
 
