05d2d8c0b9a71b612c1c4898367413766eb1d4c8
Mariano Zanetto <80788269+MarZanett@users.noreply.github.com>
2022-03-22 17:03:31 -0300

    cors_url

--- src/routes/checkOut.routes.js
+++ src/routes/checkOut.routes.js
@@ -3,19 +3,17 @@ const Stripe = require("stripe");
 const cors = require("cors");
 
 const { checkOut } = require("../controllers/checkOut.controller.js");
-const { checkOutRefund } = require("../controllers/checkoutRefund.controller.js");
-
+const {
+  checkOutRefund,
+} = require("../controllers/checkoutRefund.controller.js");
 
 const router = express();
 
-const stripe = new Stripe(
-  process.env.STRIPE_SECRET_KEY,
-);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-router.use(cors({ origin: "http://localhost:3000" }));
+router.use(cors({ origin: process.env.CORS_URL }));
 
 router.post("/", checkOut);
-router.post('/refund', checkOutRefund)
-
+router.post("/refund", checkOutRefund);
 
 module.exports = router;
