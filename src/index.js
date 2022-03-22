51a1dad0d21bdae228cef287f62488b6758e48a4
Mariano Zanetto <80788269+MarZanett@users.noreply.github.com>
2022-03-22 17:11:41 -0300

    cors_url

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const prisma = new PrismaClient();
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", process.env.CORS_URL); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", process.env.CORS_URL.toString()); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
